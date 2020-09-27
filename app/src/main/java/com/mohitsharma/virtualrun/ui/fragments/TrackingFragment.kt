d738e5ba7d895d2e7c1c35e90f525add512a65aa
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2020-09-27 22:31:25 +0530

    implemented Map view

--- app/src/main/java/com/mohitsharma/virtualrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/mohitsharma/virtualrun/ui/fragments/TrackingFragment.kt
@@ -1,13 +1,50 @@
 package com.mohitsharma.virtualrun.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.mohitsharma.virtualrun.R
 import com.mohitsharma.virtualrun.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+    private var map :GoogleMap? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
 }
