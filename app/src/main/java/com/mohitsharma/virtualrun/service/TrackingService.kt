7824155f4aef135aee74c69b9f99c7d4c20b7b3f
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2020-09-29 22:26:28 +0530

    fragment_setup.xml ui updated

---
+++ app/src/main/java/com/mohitsharma/virtualrun/service/TrackingService.kt
@@ -0,0 +1,12 @@
+package com.mohitsharma.virtualrun.service
+
+import android.content.Intent
+import androidx.lifecycle.LifecycleService
+
+class TrackingService: LifecycleService() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
