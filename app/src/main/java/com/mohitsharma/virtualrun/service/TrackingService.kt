2cc385e1b9c1b102d32c7a54093b6ae66a1b9e13
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2020-10-02 17:50:13 +0530

    notification feature for new run added

--- app/src/main/java/com/mohitsharma/virtualrun/service/TrackingService.kt
+++ app/src/main/java/com/mohitsharma/virtualrun/service/TrackingService.kt
@@ -1,13 +1,29 @@
 package com.mohitsharma.virtualrun.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.mohitsharma.virtualrun.R
 import com.mohitsharma.virtualrun.others.Constants.ACTION_PAUSE_SERVICE
+import com.mohitsharma.virtualrun.others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.mohitsharma.virtualrun.others.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.mohitsharma.virtualrun.others.Constants.ACTION_STOP_SERVICE
+import com.mohitsharma.virtualrun.others.Constants.NOTIFICATION_CHANNEL_ID
+import com.mohitsharma.virtualrun.others.Constants.NOTIFICATION_CHANNEL_NAME
+import com.mohitsharma.virtualrun.others.Constants.NOTIFICATION_ID
+import com.mohitsharma.virtualrun.ui.MainActivity
 import timber.log.Timber
 
 class TrackingService: LifecycleService() {
+    var isFirstRun = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -19,11 +35,50 @@ class TrackingService: LifecycleService() {
                     Timber.d("Stopped paused")
                 }
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Service start or resume")
+                    if (isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    }else{
+                        Timber.d("Resuming service")
+                    }
                 }
             }
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createNotificationChannel(notificationManager)
+        }
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(false)
+            .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
+            .setContentTitle("Virtual Run")
+            .setContentText("00.00.00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID,notificationBuilder.build())
+    }
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this,MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+    FLAG_UPDATE_CURRENT,
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager){
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+           IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/mohitsharma/virtualrun/ui/MainActivity.kt
+++ app/src/main/java/com/mohitsharma/virtualrun/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.mohitsharma.virtualrun.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mohitsharma.virtualrun.R
+import com.mohitsharma.virtualrun.others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         navHostFragment.findNavController()
@@ -28,5 +31,18 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_navigate_to_tracking_fragment)
+        }
+
     }
 }
