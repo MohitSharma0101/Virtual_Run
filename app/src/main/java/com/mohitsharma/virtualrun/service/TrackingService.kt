b36ea9a9239d4616f023fdd7f851d95368184f6d
MohitSharma0101 <mohitsharma.2cse23@jecrc.ac.in>
2020-10-02 16:27:09 +0530

    TrackingService added

--- app/src/main/java/com/mohitsharma/virtualrun/service/TrackingService.kt
+++ app/src/main/java/com/mohitsharma/virtualrun/service/TrackingService.kt
@@ -2,10 +2,27 @@ package com.mohitsharma.virtualrun.service
 
 import android.content.Intent
 import androidx.lifecycle.LifecycleService
+import com.mohitsharma.virtualrun.others.Constants.ACTION_PAUSE_SERVICE
+import com.mohitsharma.virtualrun.others.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.mohitsharma.virtualrun.others.Constants.ACTION_STOP_SERVICE
+import timber.log.Timber
 
 class TrackingService: LifecycleService() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let {
+            when(it.action){
+                ACTION_STOP_SERVICE -> {
+                    Timber.d("Stopped Service")
+                }
+                ACTION_PAUSE_SERVICE -> {
+                    Timber.d("Stopped paused")
+                }
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    Timber.d("Service start or resume")
+                }
+            }
+        }
 
         return super.onStartCommand(intent, flags, startId)
     }
--- app/src/main/java/com/mohitsharma/virtualrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/mohitsharma/virtualrun/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.mohitsharma.virtualrun.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import com.mohitsharma.virtualrun.R
+import com.mohitsharma.virtualrun.others.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.mohitsharma.virtualrun.service.TrackingService
 import com.mohitsharma.virtualrun.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
@@ -17,11 +20,20 @@ class TrackingFragment: Fragment(R.layout.fragment_tracking) {
     private var map :GoogleMap? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String)=
+        Intent(requireContext(),TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onLowMemory() {
         super.onLowMemory()
